691f8f0eccafea42327ba067d1541f03b30a452c
medlib.xyz <freedocs.xyz@gmail.com>
2021-02-21 17:50:24 +0200

    Debot context #0

--- QuizDebot.sol
+++ QuizDebot.sol
@@ -6,32 +6,13 @@ import "./Debot.sol";
 import "./Terminal.sol";
 
 contract QuizDebot is Debot {
-
-
-    // helper modifier
-    modifier accept() {
-        tvm.accept();
-        _;
-    }
-        // Debot context ids
-
-
-   uint public result;
-    /*
-     *   Init functions
-    */
-
+  
     constructor(uint8 options, string debotAbi, string targetAbi, address targetAddr) public {
         require(tvm.pubkey() == msg.pubkey(), 100);
         tvm.accept();
         init(options, debotAbi, targetAbi, targetAddr);
     }  
-
-    /*
-     *  Overrided Debot functions
-     */
-   
-
+    
     function fetch() public override accept returns (Context[] contexts) {}
        
 
@@ -43,7 +24,7 @@ contract QuizDebot is Debot {
     
     function quit() public override accept { }    
 
- function start() override public {
+    function start() override public {
        Terminal.print(0, "Hello from debot");
      }
    
